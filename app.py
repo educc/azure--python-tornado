26637ccac832e7e3cc6d7d00057aaf42b318c08f
educc <edward1738@gmail.com>
2017-06-20 22:43:11 -0500

    change to flask

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import tornado.ioloop
 import tornado.web
 import tornado.wsgi
+import json
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+from flask import Flask
+app = Flask(__name__)
 
-application = tornado.web.Application([(r"/", MainHandler),])
+@app.route('/')
+def hello_world():
+    return 'Hello, World from Flask'
+
+
+@app.route('/data')
+def hello_world():
+	data = [
+		{ name:"edu", age:25 },
+		{ name:"jobs", age: 56}
+	]
+    return json.dumps(data)
+###########################################################
+
+
+tr = tornado.wsgi.WSGIContainer(app)
+
+application = tornado.web.Application([
+	(r".*", FallbackHandler, dict(fallback=tr)),
+])
+
 wsgi_app  = tornado.wsgi.WSGIAdapter(application)
